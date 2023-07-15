6406da81c0b7eabeb73f7af64e292c52e70bffde
Iryna Burlaka <i.am.blinova@gmail.com>
2023-07-15 20:12:53 +0200

    fixing of task-02

--- js/task-02.js
+++ js/task-02.js
@@ -8,11 +8,13 @@ const ingredients = [
 ];
 
 const listOfIngredients = document.querySelector("#ingredients");
+const fragment = document.createDocumentFragment();
 
 ingredients.forEach((item) => {
   const ingredient = document.createElement("li");
   ingredient.classList.add("item");
   ingredient.textContent = item;
-
-  listOfIngredients.append(ingredient);
+  fragment.appendChild(ingredient);
+  console.log(fragment);
 });
+listOfIngredients.append(fragment);
